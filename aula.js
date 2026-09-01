6df96345d5db01b1d914243a519551cae0f3ab3a
stefanisantini <stefani_santini@hotmail.com>
2026-09-01 10:20:10 -0300

    aula de inicio da API e explicação de GIT e GitHub

---
+++ aula.js
@@ -0,0 +1,3 @@
+export function mensagem(a) { // export é para permiter que outro arquivo dentro do projeto use essa construção
+    console.log(`Bem-vindo a pizzaria ${a}`)
+}
---
+++ index.js
@@ -0,0 +1,6 @@
+import chalk from 'chalk';
+import { mensagem } from './aula.js';
+
+mensagem('Sté')
+
+console.log(chalk.red('Hello world!'));
